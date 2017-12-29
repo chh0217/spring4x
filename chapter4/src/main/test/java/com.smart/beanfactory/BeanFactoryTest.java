0d1d40fe3470763b11af966d42009eca6f4c56fe
chenhang <275980433@qq.com>
2017-12-29 10:11:43 +0800

    spring4.x测试

---
+++ chapter4/src/main/test/java/com.smart/beanfactory/BeanFactoryTest.java
@@ -0,0 +1,15 @@
+package com.smart.beanfactory;
+
+/**
+ * @author chenh
+ * @date 2017/12/28.
+ */
+public class BeanFactoryTest {
+    public void getBean() throws Throwable{
+//        ResourcePatternResolver
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
