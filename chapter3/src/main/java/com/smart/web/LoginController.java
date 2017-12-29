ec56bda93836bfd356a5ed93174332da24a222a9
chenhang <275980433@qq.com>
2017-12-29 10:16:59 +0800

    spring4.x测试

--- chapter3/src/main/java/com/smart/web/LoginController.java
+++ chapter3/src/main/java/com/smart/web/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
